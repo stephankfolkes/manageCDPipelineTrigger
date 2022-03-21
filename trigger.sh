d4a297fc3f029ea02728b5f0753a499e99d2c13e
Stephan Folkes <sfolkes@cloudbees.com>
2022-03-21 11:25:48 +0000

    Add initial files for creating and managing a pipeline trigger

---
+++ trigger.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get and set the webhook secret
+webhookSecret='1234abc'
+
+# Set the variables
+projectName='test'
+triggerName='test trigger'
+pipelineName='test'
+serviceAccountName='Trigger-SC'
+
+ectool createServiceAccount "$serviceAccountName" --description "Service Account for Github webhook triggers." || echo "Service account $serviceAccountName already exists."
+
+getTrigger=$(ectool getTrigger "$projectName" "$triggerName" --pipelineName "$pipelineName")
+status="$?"
+
+if [ "$status" -eq 0 ];then
+    ectool modifyTrigger "$projectName" "$triggerName" --pipelineName "$pipelineName" --triggerType 'webhook' --pluginKey 'EC-Github' --pluginParameter repositories="$(cat repositoryList)" includeBranches="$(cat includeBranches)" --serviceAccountName 'Trigger-SC' --webhookSecret "$webhookSecret"
+else
+    ectool createTrigger "$projectName" "$triggerName" --pipelineName "$pipelineName" --triggerType 'webhook' --pluginKey 'EC-Github' --pluginParameter repositories="$(cat repositoryList)" includeBranches="$(cat includeBranches)" --serviceAccountName 'Trigger-SC' --webhookSecret "$webhookSecret"
+fi
